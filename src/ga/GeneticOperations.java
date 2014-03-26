2f3d8e6466e8d83a8d8615040be41d76b3d0cf06
Butnaru Andrei-Madalin <butnaruandreimadalin@gmail.com>
2014-03-26 20:21:54 +0200

    Updated GA.

--- src/ga/GeneticOperations.java
+++ src/ga/GeneticOperations.java
@@ -1,65 +1,199 @@
 package ga;
 
+
 import org.ejml.simple.SimpleMatrix;
 
+import java.util.Arrays;
 import java.util.Random;
 
 /**
- * Created by Gabriel on 3/8/14.
+ * Created by AndreiMadalin on 3/12/14.
  */
 public class GeneticOperations {
 
     /**
-     * @param parents
-     * @param uc      probability of crossover ( usually 0.65 - 0
+     * @param parents matrice de forma m x n (m - dimensiunea unui cromozom, n - dimensiunea populatiei)
+     * @param uc      probabilitatea de crossover
+     * @return
      */
-    public static SimpleMatrix crossover(SimpleMatrix parents, int uc) {
-        int n = parents.numRows();
-        SimpleMatrix pop = new SimpleMatrix(n, parents.numCols());
+    public static SimpleMatrix singlePointCrossover(String type, SimpleMatrix parents, double uc) {
+        return crossover(type, parents, uc, 1);
+    }
 
-        Random rnd = new Random();
+    public static SimpleMatrix doublePontCrossover(String type, SimpleMatrix parents, double uc) {
+        return crossover(type, parents, uc, 2);
+    }
+
+    public static SimpleMatrix multiPointCrossover(String type, SimpleMatrix parents, double uc) {
+        Random rand = new Random();
+        int l = rand.nextInt(parents.numRows() - 3) + 3;
+
+        return crossover(type, parents, uc, l);
+    }
+
+    /**
+     * @param parents matrice de forma m x n (m - dimensiunea unui cromozom, n - dimensiunea populatiei)
+     * @param uc      probabilitatea de crossover
+     * @param l       numarul de pozitii de taiere pentru crossover
+     * @return
+     */
+    private static SimpleMatrix crossover(String type, SimpleMatrix parents, double uc, int l) {
+        int n = parents.numCols();
+        int m = parents.numRows();
+        SimpleMatrix pop = new SimpleMatrix(m, n);
         double U;
+        Random rand = new Random();
+
 
         if (n % 2 == 1) {
             n--;
+            pop.insertIntoThis(0, n, parents.extractVector(false, n));
         }
-        int j = 0;
         for (int i = 0; i < n; i += 2) {
-            U = rnd.nextDouble();
+            U = rand.nextDouble();
             if (U < uc) {
-
-                SimpleMatrix parents2 = new SimpleMatrix(2, parents.numCols());
-                parents2.insertIntoThis(0, 0, parents.extractVector(true, i));
-                parents2.insertIntoThis(1, 0, parents.extractVector(true, i + 1));
-                pop.insertIntoThis(j, 0, singlePointCrossover(parents2));
-                j += 2;
+                switch (type){
+                    case "binary": pop.insertIntoThis(0, i, multiPointBinaryCrossover(parents.extractMatrix(0, m, i, i + 2), l)); break;
+                    case "real": pop.insertIntoThis(0, i, realCrossover(parents.extractMatrix(0, m, i, i + 2))); break;
+                }
             }
-
         }
-
-        // TODO de testat
-        if (parents.numRows() % 2 == 1)
-            pop.insertIntoThis(j - 1, 0, parents.extractVector(true, n));
-
 
         return pop;
     }
 
-    private static SimpleMatrix singlePointCrossover(SimpleMatrix parents) {
-        Random rnd = new Random();
-        int cols = parents.numCols();
-        int U = rnd.nextInt(cols - 1) + 1;
+    /**
+     * @param parents matrice de forma m x n (m - dimensiunea unui cromozom, n - dimensiunea populatiei)
+     * @param l       numarul de pozitii de taiere pentru crossover
+     * @return
+     */
+    private static SimpleMatrix multiPointBinaryCrossover(SimpleMatrix parents, int l) {
+        int n = parents.numCols(); // dimensiunea populatiti
+        int m = parents.numRows(); // dimeansiunea unui cromozom
+        SimpleMatrix pop = new SimpleMatrix(parents);
+        int positions[] = generateRandom(l, m);
+        SimpleMatrix tmpA, tmpB;
 
-        SimpleMatrix pop = parents;
+        if (l % 2 == 1) {
+            l--;
 
-        SimpleMatrix tmpA,tmpB;
+            tmpA = pop.extractMatrix(positions[l], m, 0, 1);
+            tmpB = pop.extractMatrix(positions[l], m, 1, 2);
 
-        tmpA = parents.extractMatrix(0,0,U,cols);
-        tmpB = parents.extractMatrix(1,1,U,cols);
+            pop.insertIntoThis(positions[l], 0, tmpB);
+            pop.insertIntoThis(positions[l], 1, tmpA);
+        }
 
-        parents.insertIntoThis(0,U,tmpB);
-        parents.insertIntoThis(1,U,tmpA);
+        for (int i = 0; i < l; i += 2) {
+            tmpA = pop.extractMatrix(positions[i], positions[i + 1], 0, 1);
+            tmpB = pop.extractMatrix(positions[i], positions[i + 1], 1, 2);
+
+            pop.insertIntoThis(positions[i], 0, tmpB);
+            pop.insertIntoThis(positions[i], 1, tmpA);
+        }
 
         return pop;
+    }
+
+    private static SimpleMatrix realCrossover(SimpleMatrix parents){
+        int n = parents.numCols();
+        int m = parents.numRows();
+        SimpleMatrix pop = new SimpleMatrix(m,n);
+        double d = 0.25;
+        double a[], b[];
+        double tempA, tempB;
+
+        a = generateRandom(m, d, new Random());
+        b = generateRandom(m, d, new Random());
+        for (int j = 0; j < m; j++) {
+            tempA = parents.get(j, 0);
+            tempB = parents.get(j, 1);
+            pop.set(j, 0, tempA*(1 - a[j]) + tempB*a[j]);
+            pop.set(j, 1, tempA*(1 - b[j]) + tempB*b[j]);
+        }
+
+        return pop;
+    }
+
+//    public static SimpleMatrix mutation(String type, SimpleMatrix parents, double um){
+//        if(type.equals("binary"))
+//            return binaryMutation(parents, um);
+//        else
+//            return realMutation(parents, um);
+//    }
+
+    public static SimpleMatrix mutation(SimpleMatrix parents, double um){
+        Random rand = new Random();
+        int n = parents.numCols(); // dimensiunea populatiti
+        int m = parents.numRows(); // dimeansiunea unui cromozom
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(rand.nextDouble() < um){
+                    parents.set(j,i,(parents.get(j,i) == 0 ? 1 : 0));
+                }
+            }
+        }
+
+        return parents;
+    }
+
+    // TODO de parametrizat k, r ????
+    public static SimpleMatrix mutation(SimpleMatrix parents, double um, double domain){
+        Random rand = new Random();
+        Random rand2 = new Random();
+        Random rand3 = new Random();
+        int n = parents.numCols(); // dimensiunea populatiti
+        int m = parents.numRows(); // dimeansiunea unui cromozom
+        double new_val;
+        int s;
+        int k = 10; // k apartine {4,5,...,20}
+        double r = Math.pow(10,-6); // intervalul de mutatie ( r apartine [Math.pow(10,-6), 0.1] )
+        double a;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(rand.nextDouble() < um){
+                    s = Math.round(rand2.nextInt(1)) == 0 ? -1 : 1;
+                    a = Math.pow(2, -1 * rand3.nextDouble()*k);
+                    new_val = parents.get(j,i) + s * (r * domain) * a;
+                    parents.set(j,i,new_val);
+                }
+            }
+        }
+
+        return parents;
+    }
+
+    private static int[] generateRandom(int l, int m) {
+        Random rand = new Random();
+        int positions[] = new int[l];
+        int i = 0;
+        boolean g = false;
+        while (i < l) {
+            g = false;
+            positions[i] = rand.nextInt(m - 1) + 1;
+            for (int j = 0; j < i; j++)
+                if (positions[i] == positions[j]) {
+                    g = true;
+                    break;
+                }
+            if (!g)
+                i++;
+        }
+        Arrays.sort(positions);
+        return positions;
+    }
+
+    // metoda de generare a factorului de scalare pentru crossover real
+    private static double[] generateRandom(int n, double d, Random rand){
+        double low = -1 * d;
+        double high = d + 1;
+        double a[] = new double[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = (high - low) * rand.nextDouble() + low;
+        }
+
+        return a;
     }
 }
--- src/ga/Population.java
+++ src/ga/Population.java
@@ -1,46 +1,64 @@
 package ga;
-import org.ejml.*;
-import org.ejml.data.Matrix64F;
-import org.ejml.simple.SimpleMatrix;
 
+
+import org.ejml.simple.SimpleMatrix;
 import java.util.Random;
 
 /**
- * Created by  Gabriel, Andrei, Root on 3/8/14.
+ * Created by AndreiMadalin on 3/12/14.
  */
 public class Population {
-    SimpleMatrix population;
-    private int m,n;
+
     /**
-     *
-     * @param m numarul de varabile a unui cromozom
-     * @param n dimensiunea populatii de cromozomi
+     * initializare cu gene binare
+     * @param m numarul de vatiabile ale unui cromozom
+     * @param n dimensiunea populatiei de cromozomi
+     * @return pop matrice de dimensiune m x n
      */
-    public Population(int m, int n){
-        this.m = m;
-        this.n = n;
-    }
-
-    public void initBinary(){
-        population = SimpleMatrix.random(n,m,0,1,new Random());
-    }
-
-    public SimpleMatrix rouletteWheelSelection(SimpleMatrix pop, SimpleMatrix cpop){
-        int numRows = pop.numRows();
-        int numCols = pop.numCols();
-        int U;
-
-        SimpleMatrix parent = new SimpleMatrix(numRows, numCols);
-        Random rand = new Random();
-
-        for (int i = 0; i < numRows; i++) {
-            U = rand.nextInt(1);
-            for (int j = 0; j < numRows; j++) {
-                if(cpop.get(i,j - 1) <= U && U < cpop.get(i,j))
-                    parent.insertIntoThis(i,0,pop.extractVector(true, j));
+    public static SimpleMatrix init(int m, int n){
+        SimpleMatrix pop = SimpleMatrix.random(m,n,0,1,new Random());
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                pop.set(i,j,Math.round(pop.get(i,j)));
             }
         }
-        return parent;
+        return pop;
     }
 
+    /**
+     * initializarea cu variabile reale
+     * @param m numarul de vatiabile ale unui cromozom
+     * @param n dimensiunea populatiei de cromozomi
+     * @return pop matrice de dimensiune m x n
+     */
+    public static SimpleMatrix init(int m, int n, double low, double high){
+        return SimpleMatrix.random(m,n,low,high,new Random());
+    }
+
+    /**
+     *
+     * @param parent matrice de dimensiuni m x n
+     * @param cpop vector ce cotine valorile cumulate ale functiei de fitness normalizate calculate pentur fiecare individ
+     * @return parent matrice de dimensiuni m x n
+     */
+    public static SimpleMatrix rouletteWheelSelection(SimpleMatrix parent, SimpleMatrix cpop){
+        int m = parent.numRows();
+        int n = parent.numCols();
+        SimpleMatrix pop = new SimpleMatrix(m,n);
+        double U;
+        Random rand = new Random();
+
+
+        for (int i = 0; i < n; i++) {
+            U = rand.nextDouble();
+            for (int j = 0; j < n; j++) {
+                if(((j == 0)? 0 <= U : cpop.get(j - 1) <= U) && U < cpop.get(j)){
+                    pop.insertIntoThis(0, i, parent.extractVector(false, j));
+                    break;
+                }
+            }
+        }
+
+        return pop;
+    }
 }
